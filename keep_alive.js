882e9d9e401710051216da5f40a4b1beaa71c7aa
Baptiste6667 <ducruy.baptiste@gmail.com>
2026-06-21 18:56:43 +0200

    Delete keep_alive.js

--- keep_alive.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer(function (req, res) {
-  res.write("Le bot est en ligne !");
-  res.end();
-}).listen(8080, () => {
-  console.log("Serveur de maintien en vie prêt sur le port 8080");
-});
